0e4ec9d8b368635b6fc3ba54911d33cc4df5dfd1
Hardik Sharma <hardiksharmmaaaa@users.noreply.github.com>
2026-08-20 15:34:47 +0400

    Initial Commit

---
+++ ku-onboardingTests/ku_onboardingTests.swift
@@ -0,0 +1,19 @@
+//
+//  ku_onboardingTests.swift
+//  ku-onboardingTests
+//
+//  Created by Hardik Sharma on 20/08/26.
+//
+
+import Testing
+@testable import ku_onboarding
+
+struct ku_onboardingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
